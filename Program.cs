9fff1a77a4a6af23825ff95f8f32023bbb4e06b9
Tobias <72868238+Busky-B@users.noreply.github.com>
2021-02-08 11:01:06 +0100

    Trying out Git-Bash!!!

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
 		static void Main(string[] args)
 		{
 			Console.WriteLine("I will push into master!");
+			Console.WriteLine("I was written from code");
 			//please make fetch and merge
 			//temp change
 			
